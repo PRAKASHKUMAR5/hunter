e409659e9c1ef58e1e790e3734989e31e8189d4b
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-05-25 11:42:13 +0530

    Create 5 Number of way to print.cpp

---
+++ 5 Number of way to print.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,c,count=1,x;
+    cin>>a;
+    string str=to_string(a);
+    int l=str.length();
+    char b;
+    for(int i=0;i<l-1;i+=1)
+    {
+        if(str[i]=='0')
+        {
+            count--;
+        }
+        c=0;
+        x=str[i];
+        x-=48;
+        x*=10;
+        b=x+str[i+1];
+        c=b-48;
+        cout<<"c="<<c<<endl;
+        if(c<=26)
+            {
+                count++;
+            }
+     }
+     cout<<count;
+   
+}
